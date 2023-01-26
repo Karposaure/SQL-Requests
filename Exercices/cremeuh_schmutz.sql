73f8213fc8e3bd5b867c6275d3430e34be22361f
Florian Schmutz <florian.schmutz@heig-vd.ch>
2023-01-26 09:53:31 +0100

    new file

---
+++ Exercices/cremeuh_schmutz.sql
@@ -0,0 +1,40 @@
+-- ---------------------------------------------------------------------------
+-- ---------------------------------------------------------------------------
+-- script :        schema.sql
+-- database:       cremeuh
+-- contributors :  Paul Jolles (pjs)
+--                 Gabor Maksay (gmy)
+-- history :       when       | ver | who | what
+--                 2023-01-12 | 1.1 | gmy | adapted queries due to sr diagram modifications
+--                 2022-12-07 | 1.0 | pjs | creation
+
+-- ---------------------------------------------------------------------------
+-- ------------------------------------------------------------------ requetes
+
+-- 1. L'id, la date de mise en cave et le nom de la fromagerie des meules de
+-- fromage encore en cave, triées par ordre descendant de la date de mise en cave.
+-- A COMPLETER
+
+-- 2. L'id des meules de fromage qui étaient en cave le 21 février 2020 à la
+-- 'Fromagerie du lac' située dans le canton de Vaud, affichés par ordre ascendant des ids.
+-- A COMPLETER
+
+-- 3. L'id et le nom des vaches actuellement présentes dans l'élevage 
+-- 'Les vaches des alpes' situé dans le canton de Vaud, affichés par ordre 
+-- alphabétique du nom des vaches.
+-- A COMPLETER
+
+-- 4. Le nombre de meules de fromages provenant de la fromagerie 'Fromagerie Breguet' 
+-- située dans le canton Vaud et ayant bénéficié de 16 mois d'affinage.
+-- A COMPLETER
+
+-- 5. La quantité - volume - de lait utilisée en 2021 par la fromagerie 
+-- 'Fromagerie Breguet' située dans le canton de Vaud.
+-- A COMPLETER
+
+-- 6. Toutes les informations sur les traites de lait qui n'ont
+-- pas été utilisées pour confectionner des meules de fromage.
+-- A COMPLETER
+
+-- 7. La mère de la vache dont l'id est égal à 1078.
+-- A COMPLETER
